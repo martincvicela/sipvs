a79718c356e7d3852d2552152bcfdf079c345b3d
Ivan Gulis <gulis2505@gmail.com>
2017-10-18 18:56:27 +0200

    Added output to file
    Added output to file

--- src/Signature.java
+++ src/Signature.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import sk.ditec.zep.dsigner.xades.XadesSig;
 import sk.ditec.zep.dsigner.xades.plugin.DataObject;
@@ -20,14 +21,16 @@ public class Signature extends AbstractSignature {
 		DataObject xmlObject = xmlPlugin.createObject(
 				"XML1", 
 				"XML", 
+				//readResource("C:/skola9/SIPVS/Git/sipvs/file.xml"),
+				//readResource("C:/skola9/SIPVS/Git/sipvs/file.xsd"),
 				readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xml"),
 				readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsd"), 
 				"",
 				"http://www.w3.org/2001/XMLSchema",
+				//readResource("C:/skola9/SIPVS/Git/sipvs/file.xsl"),
 				readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsl"),
 				"http://www.w3.org/1999/XSL/Transform");
 
-		System.out.println("Hello, World");
 		if (xmlObject == null) {
 			System.out.println("XMLPlugin.createObject() errorMessage=" + xmlPlugin.getErrorMessage());
 			return;
@@ -47,6 +50,10 @@ public class Signature extends AbstractSignature {
 		}		
 
 		System.out.println(dSigner.getSignedXmlWithEnvelope());
+		PrintWriter out = new PrintWriter("signedXml.xml");
+		out.println(dSigner.getSignedXmlWithEnvelope());
+		out.close();
+		
 	}
 	
 }
