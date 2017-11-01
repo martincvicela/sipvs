83717fb472af65064f710c684dc9ddff5cab6d3d
Ivan Gulis <gulis2505@gmail.com>
2017-11-01 19:40:06 +0100

    First functional version - ojebané
    First functional version - ojebané

--- src/Signature.java
+++ src/Signature.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
@@ -13,6 +14,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -43,6 +46,7 @@ public class Signature extends AbstractSignature {
 		}
 	
 	static public void sign(List<String> documentNames) throws IOException {
+		int count = 1;
 		int rc = -1;		
 		XadesSig dSigner = new XadesSig();
 		dSigner.installLookAndFeel();
@@ -57,14 +61,14 @@ public class Signature extends AbstractSignature {
 			DataObject xmlObject = xmlPlugin.createObject2(
 					documentNames.get(i).replaceAll("[.]", ""), 
 					MainGUI.nazov, 
-					//readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/" + documentNames.get(i)),
-					//readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsd"), 
-					readResource("c:/skola9/SIPVS/Git/sipvs/" + documentNames.get(i)),
-					readResource("c:/skola9/SIPVS/Git/sipvs/file.xsd"), 
+					readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/" + documentNames.get(i)),
+					readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsd"), 
+					//readResource("c:/skola9/SIPVS/Git/sipvs/" + documentNames.get(i)),
+					//readResource("c:/skola9/SIPVS/Git/sipvs/file.xsd"), 
 					"",
 					"http://www.w3.org/2001/XMLSchema",
-					//readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsl"),
-					readResource("c:/skola9/SIPVS/Git/sipvs/file.xsl"),
+					readResource("C:/Users/mato1/OneDrive/SIPVS/zadanie/file.xsl"),
+					//readResource("c:/skola9/SIPVS/Git/sipvs/file.xsl"),
 					"http://www.w3.org/1999/XSL/Transform",
 					"HTML");
 
@@ -82,7 +86,7 @@ public class Signature extends AbstractSignature {
 			return;
 		}
 
-		rc = dSigner.sign20(makeid(), "http://www.w3.org/2001/04/xmlenc#sha256", "urn:oid:1.3.158.36061701.1.2.1", "dataEnvelopeId",
+		rc = dSigner.sign20("SignId" + count, "http://www.w3.org/2001/04/xmlenc#sha256", "urn:oid:1.3.158.36061701.1.2.1", "dataEnvelopeId",
 				"dataEnvelopeURI", "dataEnvelopeDescr");
 		if (rc != 0) {
 			System.out.println("XadesSig.sign20() errorCode=" + rc + ", errorMessage=" + dSigner.getErrorMessage());
@@ -100,17 +104,37 @@ public class Signature extends AbstractSignature {
 		} catch (SAXException | ParserConfigurationException e) {
 			e.printStackTrace();
 		}
+
+		//get TimeStampValue by Web Service
+		String timeStampValue = getTimeStamp(document.getElementsByTagName("ds:SignatureValue").item(0).getTextContent());
 		
-		Node signatureValue = document.getElementsByTagName("ds:SignatureValue").item(0);
+		Element unsignedProperties = document.createElement("xades:UnsignedProperties");
 		
+		Element unsignedSignatureProperties = document.createElement("xades:UnsignedSignatureProperties");
 		
-		String timeStampValue = getTimeStamp(signatureValue.getTextContent());
-		System.out.println(timeStampValue); //zatial vypÌsaù zÌskan˙ peËiatku, nevieme eöte Ëo v nej bude
+		Element signatureTimestamp = document.createElement("xades:SignatureTimeStamp");
+		signatureTimestamp.setAttribute("Id", "TSID" + count++);
 		
-		/*PrintWriter out = new PrintWriter("signedXml.xml");
-		out.println(dSigner.getSignedXmlWithEnvelope());
-		out.close();	*/	
+		Element encapsulatedTimeStamp = document.createElement("xades:EncapsulatedTimeStamp");
+
+		unsignedProperties.appendChild(unsignedSignatureProperties);
+		unsignedSignatureProperties.appendChild(signatureTimestamp);
+		signatureTimestamp.appendChild(encapsulatedTimeStamp);
+		encapsulatedTimeStamp.appendChild(document.createTextNode(timeStampValue));
+		document.getElementsByTagName("xades:QualifyingProperties").item(0).appendChild(unsignedProperties);
 		
+		try {
+			//Generate XML file
+			Source xmlSource = new DOMSource(document);
+			Result result = new StreamResult(new FileOutputStream("signedWithStamp.xml"));
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty("indent", "yes");
+			transformer.transform(xmlSource, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 	
 	static public String getTimeStamp(String xmlData) {
--- src/TimeStamp.java
+++ src/TimeStamp.java
@@ -2,161 +2,93 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.tsp.TimeStampResp;
 import org.bouncycastle.tsp.TSPAlgorithms;
 import org.bouncycastle.tsp.TSPException;
 import org.bouncycastle.tsp.TimeStampRequest;
 import org.bouncycastle.tsp.TimeStampRequestGenerator;
 import org.bouncycastle.tsp.TimeStampResponse;
-//import org.bouncycastle.tsp.TimeStampRequest;
-import org.bouncycastle.tsp.TimeStampToken; //https://www.bouncycastle.org/latest_releases.html
+import org.bouncycastle.tsp.TimeStampToken;
 
-//nepoužívajú tu TimeStampRequest ale miesto toho asi ditec.TS?
-//import sk.ditec.TS;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import org.apache.cxf.*;
-import java.net.URL;
+import org.apache.cxf.helpers.IOUtils;
 
-import javax.xml.bind.Element;
-import javax.xml.namespace.QName;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Service;
-import javax.xml.ws.handler.MessageContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class TimeStamp {
 	
 	public String getTS(String input) {
-		
-		//náhrada za import sk.ditec.TS;
-		URL wsdlURL = null;
+		     
+        String base64data = Base64.getEncoder().encodeToString(input.getBytes());
+        
+        String rawOutput = null;
 		try {
-			wsdlURL = new URL("http://test.ditec.sk/timestampws/TS.asmx?wsdl");
-		} catch (MalformedURLException e) {
+			rawOutput = IOUtils.readStringFromStream(getWholeTimeStamp(base64data));
+			System.out.println("Service output: " + rawOutput);
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = null;
+		InputSource source = new InputSource(new StringReader(rawOutput));
+		Document document = null;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+			document = docBuilder.parse(source);
+		} catch (SAXException | ParserConfigurationException | IOException e) {
 			e.printStackTrace();
 		}
-		Service service = Service.create(wsdlURL, new QName("http://www.ditec.sk/", "TS"));
-		Dispatch<Source> disp = service.createDispatch(new QName("http://www.ditec.sk/", "TSSoap"), Source.class, Service.Mode.MESSAGE);
 		
-		Map<String, Object> headers = new HashMap<String, Object>();
-	    headers.put("Content-Type", Arrays.asList(new String[] {"application/timestamp-query"}));
-		disp.getRequestContext().put(MessageContext.HTTP_REQUEST_METHOD, "POST");
-		disp.getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+		Node timeStampResult = document.getElementsByTagName("GetTimestampResult").item(0);
 		
-		Source response = (Source)disp.invoke(new StreamSource(new StringReader(input)));
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
-		String timeStampB64 = response.toString();
-		
-		TimeStampResponse TSresponse = null;
-		try {
-			TSresponse = new TimeStampResponse(Base64.getDecoder().decode(timeStampB64.getBytes()));
-		} catch (TSPException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		TimeStampToken timestampToken = TSresponse.getTimeStampToken();
-		   
-		/*Element docElement = DOMUtils.sourceToElement(retObj);
-		Element retElement = DOMUtils.getFirstChildElement(docElement);
-		String retPayload = DOMWriter.printNode(retElement, false);*/
-	    
-		//Source request = new StreamSource(input);
-		//String stringRequest = Base64.getEncoder().encodeToString(input.getBytes("utf-8"));
-		//Source response = disp.invoke(request);
-		//String response = disp.invoke(stringRequest);
-		//System.out.println(response);
-		//System.out.println(((Map<String, Object>) disp.getResponseContext().get(MessageContext.HTTP_RESPONSE_HEADERS)).get(null));
-		
-		try {
-			System.out.println(new String(Base64.getEncoder().encode(timestampToken.getEncoded())));
-			return new String(Base64.getEncoder().encode(timestampToken.getEncoded()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "";
+		return timeStampResult.getTextContent();
 	}
 
-	/*public String getTS(String input) {
-		
-		//String stringRequest = Base64.getEncoder().encodeToString(input.getBytes("utf-8"));
-		//String stringResponse = getTSAResponse(stringRequest);
-		///////////
-		try {
-			MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-			messageDigest.update(input.getBytes("utf-8"));
-			byte[] digest = messageDigest.digest();
-			
-			//byte[] responseByteData = Base64.decode(stringResponse.getBytes());
-			//InputStream in = new ByteArrayInputStream(responseByteData);
-			
-			TimeStampRequestGenerator reqgen = new TimeStampRequestGenerator();
-			TimeStampRequest req = reqgen.generate(TSPAlgorithms.SHA1, digest);
-			byte request[] = req.getEncoded();
-
-			InputStream in = getStandardTSAREsponse(request);
-			//InputStream in = getDitecTSAResponse(request);
-			System.out.println(in);
-
-			TimeStampResp resp = TimeStampResp.getInstance(new ASN1InputStream(in).readObject());
-			TimeStampResponse response = new TimeStampResponse(resp);
-			response.validate(req);
-			System.out.println(response.getTimeStampToken().getTimeStampInfo().getGenTime());
-			
-			
-			
-		} catch (NoSuchAlgorithmException | IOException | TSPException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-		return "";
-	}*/
-
-	/*private static InputStream getStandardTSAREsponse(byte request[]) {
+	private static InputStream getWholeTimeStamp(String base64data) {
 		InputStream in = null;
 		try {
-
 			OutputStream out = null;
-			URL url = new URL("http://test.ditec.sk/timestampws/TS.asmx");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setDoOutput(true);
-			con.setDoInput(true);
-			con.setRequestMethod("POST");
-			con.setRequestProperty("Content-type", "application/timestamp-query");
-			con.setRequestProperty("Content-length", String.valueOf(request.length));
-			out = con.getOutputStream();
-			out.write(request);
-			out.flush();
-
-			if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
-				throw new IOException(
-						"Received HTTP error: " + con.getResponseCode() + " - " + con.getResponseMessage());
-			}
+			URL myUrl = new URL("http://test.ditec.sk/timestampws/TS.asmx");
+			HttpURLConnection connection = (HttpURLConnection) myUrl.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setRequestMethod("POST");
+			//connection.setRequestProperty("Content-type", "application/timestamp-query");
+			connection.setRequestProperty("Content-type", "text/xml; charset=utf-8");
+			connection.setRequestProperty("SOAPAction", "http://www.ditec.sk/GetTimestamp");
 			
-			in = con.getInputStream();
+			out = connection.getOutputStream();
+			Writer wout = new OutputStreamWriter(out);
+			wout.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                    "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                    "  <soap:Body>\n" +
+                    "    <GetTimestamp xmlns=\"http://www.ditec.sk/\">\n" +
+                    "      <dataB64>" + base64data + "</dataB64>\n" +
+                    "    </GetTimestamp>\n" +
+                    "  </soap:Body>\n" +
+                    "</soap:Envelope>");
+			wout.flush();
+			
+			in = connection.getInputStream();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return in;
-	}*/
+	}
 	
 }
