80f6677ada56c466a0ac3ae866b16cf080e13844
Mato <mato155@gmail.com>
2017-11-15 08:02:25 +0100

    added third rule

--- src/Validator.java
+++ src/Validator.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,40 +8,19 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Validator {
 	private  Document parsedDoc;
 	private XPath xpath;
 	
-	
-	/*I changed my mind :)
-	public enum Rules implements Rule{
-	    RULE1 {
-	        public boolean verifie() {
-	        	System.out.println("rule1");
-	        	return true;
-	        }
-	    }, RULE2 {
-	        public boolean verifie() {
-	        	System.out.println("rule2");
-	        	return true;
-	        }
-	    }, RULE3 {
-	        public boolean verifie() {
-	        	System.out.println("rule3");
-	        	return true;
-	        }
-	    };
-
-	}*/
 	private Rule[] rules = new Rule[] {
 			
 			/*
@@ -82,10 +60,10 @@ public class Validator {
 	        		signatureMethods.add("http://www.w3.org/2001/04/xmldsig-more#rsa-sha512");
 	        		canonicalizationMethods.add("http://www.w3.org/TR/2001/REC-xml-c14n-20010315");
 	        		
-	        		Element e = (Element) xpath.evaluate("//ds:Signature/ds:SignedInfo/ds:SignatureMethod", parsedDoc, XPathConstants.NODE);
-	        		//if (e == null) return false;
-	        		if (signatureMethods.contains(e.getAttribute("Algorithm"))
-	        				&& canonicalizationMethods.contains("Algorithm"))
+	        		Node e = parsedDoc.getElementsByTagName("ds:SignatureMethod").item(0);
+	        		Node f = parsedDoc.getElementsByTagName("ds:CanonicalizationMethod").item(0);
+	        		if (signatureMethods.contains(e.getAttributes().getNamedItem("Algorithm").getNodeValue())
+	        				&& canonicalizationMethods.contains(f.getAttributes().getNamedItem("Algorithm").getNodeValue()))
 	        		{
 	        			return true;
 	        		}
@@ -101,10 +79,32 @@ public class Validator {
 	        { 
 	        	public boolean verifie() 
 	        	{ 
+	        		Set<String> transform = new HashSet<String>();
+	        		Set<String> digestMethod = new HashSet<String>();
+	        		transform.add("http://www.w3.org/TR/2001/REC-xml-c14n-20010315"); 
+	        		digestMethod.add("http://www.w3.org/2000/09/xmldsig#sha1");
+	        		digestMethod.add("http://www.w3.org/2001/04/xmldsig-more#sha224");
+	        		digestMethod.add("http://www.w3.org/2001/04/xmlenc#sha256");
+	        		digestMethod.add("http://www.w3.org/2001/04/xmldsig-more#sha384");
+	        		digestMethod.add("http://www.w3.org/2001/04/xmlenc#sha512");
+	        		
+	        		NodeList e = parsedDoc.getElementsByTagName("ds:Transform");
+	        		NodeList f = parsedDoc.getElementsByTagName("ds:DigestMethod");
+	        		for (int i = 0; i < e.getLength(); i++) {
+	        			if (!transform.contains(e.item(i).getAttributes().getNamedItem("Algorithm").getNodeValue())) 
+	        			{
+	        				return false;
+	        			}
+	        		}
+	        		for (int i = 0; i < f.getLength(); i++) {
+	        			if (!digestMethod.contains(f.item(i).getAttributes().getNamedItem("Algorithm").getNodeValue())) 
+	        			{
+	        				return false;
+	        			}
+	        		}
 	        		return true;
 	        	} 
-	        },
-	        new Rule() { public boolean verifie() { return true; } },
+	        }
 	    };
 
 	public interface Rule{
@@ -115,13 +115,13 @@ public class Validator {
 	Validator(File xmlFile) throws ParserConfigurationException, SAXException, IOException
 	{
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		parsedDoc = builder.parse(xmlFile);
 		xpath = XPathFactory.newInstance().newXPath();
 	}
 	
-	
-	
+		
 	int validate()
 	{
 		for(int i = 0; i< rules.length; i++)
